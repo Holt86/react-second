7aab037630c8aa5dd58189001e7a7db294b2079a
A.Ovechnikov <ayovechnikov.gmail.com>
2019-01-21 17:21:51 +0300

    fix init commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import Header from './Components/Header'
-import Technologies from './Components/Technologies'
-import Footer from './Components/Footer'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Technologies />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className='app-wrapper'>
+                <header className='header'>
+                    <img src='https://yandex.ru/collections/card/5b9b472acf25b67372ad7f20/'></img>
+                </header>
+                <nav className='nav'>
+                    <div>Profile</div>
+                </nav>
+                <div className='content'>
+                    Main Content
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
